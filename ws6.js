9d21488d7c68deea546efaaf509a10c6d28756d6
mualado103 <mualado84@gmail.com>
2016-11-16 02:05:01 +0700

    Create ws6.js

---
+++ ws6.js
@@ -0,0 +1,10 @@
+function passCustom(){ 
+	simpleCart.checkoutTo = Custom;
+	simpleCart.email = &quot;mualado103@gmail.com&quot;
+	simpleCart.currency = VND;
+	simpleCart.successUrl = &quot;http://www.dienthoaidailoan.net/&quot;
+} 
+function passGoogle(){ 
+	simpleCart.checkoutTo = Email; 		
+	simpleCart.currency = VND;
+}
